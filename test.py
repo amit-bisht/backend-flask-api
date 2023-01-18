1b2e65c19470588505838eff64a97d5e99a6ff04
amit-bisht <bishtakb69@gmail.com>
2023-01-18 09:35:38 +0530

    test file modified

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 import requests
 class FlaskTest(unittest.TestCase):
     URL="http://127.0.0.1:5000/api/user/"
-    REPO_URL="http://127.0.0.1:5000/api/repo/"
+    REPO_URL="http://127.0.0.1:5000/api/repos/"
     def test_userinfo_success(self,name):
         self.URL+=name
         resp=requests.get(self.URL)
@@ -14,14 +14,20 @@ class FlaskTest(unittest.TestCase):
         resp=requests.get(self.URL)
         self.assertEqual(resp.status_code,404)
         print("Test completed")
-    def test_user_repo(self,name):
+    def test_user_repo_success(self,name):
         self.REPO_URL+=name
         resp=requests.get(self.REPO_URL)
         self.assertEqual(resp.status_code,200)
+        print("Test completed")
+    def test_user_repo_fail(self,name):
+        self.REPO_URL+=name
+        resp=requests.get(self.REPO_URL)
+        self.assertEqual(resp.status_code,404)
         print("Test completed")
 if __name__=="__main__":
     tester=FlaskTest()
     tester.test_userinfo_success('amit-bisht')
     tester.test_userinfo_fail("wrong-name")
-    tester.test_user_repo("raj")
+    tester.test_user_repo_success("raj")
+    tester.test_user_repo_fail("wrong-name")
 
